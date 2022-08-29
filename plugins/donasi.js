c54ecc3deb69c947fb368f681b1e5edd4d40541a
XiaomiBOT <112295130+XiaomiBOT@users.noreply.github.com>
2022-08-29 08:26:44 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Indosat [085779150501]
+│ • Gopay  [085779150501]
+│ • Dana  [085779150501]
+│ • Saweria  [https://saweria.co/ranggabot123]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/62857791505012
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
